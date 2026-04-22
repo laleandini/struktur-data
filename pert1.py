f1431adae386ea1760cab6c8d19bae4285fe42d2
laleandini <lalekartikaayuandini86@gmail.com>
2026-04-22 12:07:09 +0700

    pert1.py

---
+++ pert1.py
@@ -0,0 +1,13 @@
+nama = input("masukkan nama anda : ")
+if nama == "andin" : print("selamat datang cantik")
+print("program selesai")
+umur = int(input("masukkan umur anda : "))
+if umur <= 0 : 
+    print("anda belum lahir")
+elif umur >= 60 : 
+    print("banyakin ibadah, bentar lagi mati")
+elif umur >= 18 : 
+    print("anda cukup umur")
+else:
+    print("anda belum cukup umur")
+print("program selesai")
